02570a5fdf3b4299c0e753afc0bd2ae8ef59d4ad
SandroBarillaPXL <sandrobarilla2001@gmail.com>
2024-11-26 09:40:09 +0100

    reorg files

--- startup.ps1
+++ startup.ps1
@@ -1,10 +1,10 @@
 k3d cluster create expertlab -p "8080:80@loadbalancer"
-kubectl apply -f .\namespace.yml
-kubectl apply -f .\deployments.yml
-kubectl apply -f .\services.yml
-kubectl apply -f .\ingress.yml
-kubectl apply -f .\configmaps.yml
-kubectl apply -f .\secrets.yml
+kubectl apply -f .\k8s\namespace.yml
+kubectl apply -f .\k8s\deployments.yml
+kubectl apply -f .\k8s\services.yml
+kubectl apply -f .\k8s\ingress.yml
+kubectl apply -f .\k8s\configmaps.yml
+kubectl apply -f .\k8s\secrets.yml
 Write-Host "`n`The app is now available on http://localhost:8080" -ForegroundColor Green
 [system.Diagnostics.Process]::Start("chrome","http://localhost:8080/home.html")
 Write-Host "`n`Press ENTER to exit and delete cluster ..." -ForegroundColor Red
